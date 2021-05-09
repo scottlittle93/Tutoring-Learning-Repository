00d125070e0a9fd4a5a1aa6952eb083479a4c537
scottlittle93 <49256128+scottlittle93@users.noreply.github.com>
2021-05-09 10:06:18 -0500

    Add 3 new routes inside Test.js Route
    Add 3 new routes inside Test.js Route

--- routes/Test.js
+++ routes/Test.js
@@ -8,5 +8,29 @@ router.get("/test", (req, res) => {
     });
 });
 
+router.get("/testtwo", (req, res) => {
+res.json({
+    key1: "value1",
+    key2: "value2"
+});
+});
+
+
+//Remember to change to "PUT" inside Postman
+router.put("/testput", (req, res) => {
+    res.json({
+        key1: "put value response 1",
+        key2: "put value response 2"
+    });
+});
+
+router.post("/testpost", (req, res) => {
+    res.json({
+        key1: "post #1",
+        key2: "post #2"
+    });
+});
+
 module.exports = router;
 
+//Question for Ryan - are each of these considered a route, or is the whole file a "route"? 
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+//Routes
 const testRoute = require("./routes/Test")
 
+
+
+
+//Initialize Express
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,10 +16,10 @@ app.use(bodyParser.json());
 
 
 //Routes go here
-//first argument is destination for where to put the contents (URL, postman), second argument is what content to put there
 //first argument sets the URL for the route, second argument is the contents of the route. But the overall URL will be the first argument + any additional URL "/" stuff that is found in the contents of the route.
 
 app.use("/", testRoute);
+
 
 const port = process.env.PORT || 5000;
 
